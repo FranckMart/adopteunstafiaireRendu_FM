66a1727b224a83ff127ceff7d194136fc3741769
Franck <franckmart22@gmail.com>
2020-03-17 09:33:17 +0100

    first commit

---
+++ include/form.php
@@ -0,0 +1,33 @@
+<form>
+                <div class="formTop">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1"><h3>Email address</h3></label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1"><h3>Password</h3></label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                </div>
+                <div class="formCenter">
+                    <div class="form-group-middle">
+                        <label for="exampleInputPassword1"><h3>Password</h3></label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                </div>
+                <div class="formBottom">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1"><h3>Email address<h3></label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1"><h3>Password</h3></label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                </div>
+                <div class="contentBtn">
+                    <button type="submit" class="btn-submit">Submit</button>
+                </div>
+
+            </form>
